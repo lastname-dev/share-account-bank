cafdd6a6dafee78c77cdb76e367848ccfc491bda
이석준 <sukjune96@gmail.com>
2023-08-31 21:06:00 +0900

    :art: Style: 전역스타일링 설정

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./styles/globalStyle";
+import { theme } from "./styles/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <App />
+  </ThemeProvider>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/styles/theme.js
@@ -0,0 +1,20 @@
+const color = {
+  lightgray: "#d9d9d9",
+  darkgray: "#454545",
+  red: "#F86375",
+  blue: "#6180D2",
+  black: "black",
+  white: "white",
+};
+
+const fontSize = {
+  s: "1.6rem",
+  m: "2rem",
+  l: "2.5rem",
+  xl: "3.5rem",
+};
+
+export const theme = {
+  color,
+  fontSize,
+};
