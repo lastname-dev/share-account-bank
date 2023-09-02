513c81e94d3268c16188ed280566cd31901e2091
이석준 <sukjune96@gmail.com>
2023-09-03 03:17:29 +0900

    :art: Style : 헤더 아이콘 설정

--- client/src/components/Layout/Header/Header.jsx
+++ client/src/components/Layout/Header/Header.jsx
@@ -1,11 +1,22 @@
+import { MdArrowBackIosNew, MdOutlineNotifications } from "react-icons/md";
 import { useLocation } from "react-router-dom";
 import * as S from "./Header.style";
+import { theme } from "styles/theme";
 
 const Header = () => {
   const { pathname } = useLocation();
 
-  if (pathname === "/main") return <S.HeaderWrapper>메인</S.HeaderWrapper>;
+  if (pathname === "/main")
+    return (
+      <S.HeaderWrapper $isMain={pathname === "/main"}>
+        <MdOutlineNotifications size={"3rem"} color={theme.color.darkgray} />
+      </S.HeaderWrapper>
+    );
 
-  return <S.HeaderWrapper>뒤로가기 존재</S.HeaderWrapper>;
+  return (
+    <S.HeaderWrapper $isMain={pathname === "/main"}>
+      <MdArrowBackIosNew size={"3rem"} color={theme.color.darkgray} />
+    </S.HeaderWrapper>
+  );
 };
 export default Header;
--- client/src/components/Layout/Header/Header.style.js
+++ client/src/components/Layout/Header/Header.style.js
@@ -2,7 +2,10 @@ import Flex from "components/@common/Flex/Flex";
 import { styled } from "styled-components";
 
 export const HeaderWrapper = styled(Flex)`
-  justify-content: space-between;
-  padding: 0.5rem 3rem;
+  justify-content: ${({ $isMain }) => ($isMain ? "end" : "start")};
+  padding: 1rem 3rem;
   position: sticky;
+  top: 0;
+  background-color: ${({ theme }) => theme.color.white};
+  border-bottom: 1px solid rgba(0, 27, 55, 0.1);
 `;
