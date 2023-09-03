ecc6e221192681ffd7dd18cf94ac5f4384b919b4
Choi Hwi bin <chlgnlqls3@nate.com>
2023-09-03 22:36:26 +0900

    ✨Feat: RegistAccountPage

--- client/src/pages/RegistAccountPage/RegistAccountPage.style.js
+++ client/src/pages/RegistAccountPage/RegistAccountPage.style.js
@@ -1,9 +1,34 @@
 import { styled } from "styled-components";
 import Flex from "components/@common/Flex/Flex";
+import Button from "components/@common/Button/Button";
 
 export const RegistAccountPageWrapper = styled(Flex)`
   flex-direction: column;
-  justify-content: space-between;
   height: 100vh;
   padding: 7rem 0;
+  gap: 6rem;
+`;
+export const RegistAccountText = styled.span`
+  font-size: ${({ theme }) => theme.fontSize.l};
+  color: ${({ theme }) => theme.color.darkgray};
+  font-weight: bold;
+`;
+
+export const InputWrapper = styled(Flex)`
+  flex-direction: column;
+  justify-content: space-around;
+  gap: 3.5rem;
+`;
+
+export const InputBox = styled.input`
+  padding: 0.5rem 1rem;
+  border-bottom: solid 1px ${({ theme }) => theme.color.darkgray};
+  width: 60%;
+`;
+
+export const NextButton = styled(Button)`
+  width: 60%;
+  background-color: ${({ theme }) => theme.color.primary};
+  color: ${({ theme }) => theme.color.white};
+  font-size: ${({ theme }) => theme.fontSize.m};
 `;
