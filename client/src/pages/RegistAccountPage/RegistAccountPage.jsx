83c83db96cb3dd1739e79ea1ee8c6a0067c0305f
Choi Hwi bin <chlgnlqls3@nate.com>
2023-09-03 18:31:20 +0900

    ✨Feat: RegistAccount 페이지 추가 및 Approunter 수정

---
+++ client/src/pages/RegistAccountPage/RegistAccountPage.jsx
@@ -0,0 +1,6 @@
+import * as S from "./RegistAccountPage.style";
+
+const RegistAccountPage = () => {
+  return <S.RegistAccountPageWrapper></S.RegistAccountPageWrapper>;
+};
+export default RegistAccountPage;
