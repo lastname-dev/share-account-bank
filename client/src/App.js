26c271355273175496a5a2152008d2c11aeff884
이석준 <sukjune96@gmail.com>
2023-09-03 20:27:22 +0900

    :sparkles: Feat : React-query Provider 설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import AppRouter from "router/AppRouter";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
     <React.Fragment>
-      <AppRouter />
+      <QueryClientProvider client={queryClient}>
+        <AppRouter />
+      </QueryClientProvider>
     </React.Fragment>
   );
 }
