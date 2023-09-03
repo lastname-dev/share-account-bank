aa66cb07fcd6d4e936c9e951e95ea4acd8800863
Choi Hwi bin <chlgnlqls3@nate.com>
2023-09-03 18:31:20 +0900

    ✨Feat: RegistAccount 페이지 추가 및 Approunter 수정

---
+++ client/src/pages/RegistAccountPage/RegistAccountPage.style.js
@@ -0,0 +1,9 @@
+import { styled } from "styled-components";
+import Flex from "components/@common/Flex/Flex";
+
+export const RegistAccountPageWrapper = styled(Flex)`
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100vh;
+  padding: 7rem 0;
+`;
