62bf62a4d8cceff69ea39ae229f97eac1c788a69
Choi Hwi bin <chlgnlqls3@nate.com>
2023-09-03 22:36:26 +0900

    ✨Feat: RegistAccountPage

--- client/src/pages/RegistAccountPage/RegistAccountPage.jsx
+++ client/src/pages/RegistAccountPage/RegistAccountPage.jsx
@@ -1,6 +1,19 @@
 import * as S from "./RegistAccountPage.style";
 
 const RegistAccountPage = () => {
-  return <S.RegistAccountPageWrapper></S.RegistAccountPageWrapper>;
+  return (
+    <S.RegistAccountPageWrapper>
+      <S.RegistAccountText>모임통장 만들기</S.RegistAccountText>
+      <S.InputWrapper>
+        <S.InputBox placeholder="모임 이름" type="text" />
+        <S.InputBox placeholder="계좌" type="email" />
+        <S.InputBox placeholder="목표금액" type="text" />
+        <div>여행예정일</div>
+        <S.InputBox placeholder="여행예정일" type="date" />
+        <S.InputBox placeholder="참여인원" type="number" />
+      </S.InputWrapper>
+      <S.NextButton>다음</S.NextButton>
+    </S.RegistAccountPageWrapper>
+  );
 };
 export default RegistAccountPage;
