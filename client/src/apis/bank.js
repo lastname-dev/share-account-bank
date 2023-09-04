c02c58ba56034582fee57c626c89e1755c1a89e9
이석준 <sukjune96@gmail.com>
2023-09-04 20:08:20 +0900

    :sparkles: Feat : 계좌관련 API 추가

--- client/src/apis/bank.js
+++ client/src/apis/bank.js
@@ -3,6 +3,11 @@ import { axiosInstance } from "./index";
 const bankAPI = {
   verifyAccount: (name, account) => axiosInstance.post("/accounts/verification", { name, account }),
 
+  transactAccount: (sender, receiver, amount) =>
+    axiosInstance.post("/accounts/transactions", { sender, receiver, amount }),
+
+  depositAccount: (userId, amount) => axiosInstance.post("/accounts/deposit", { userId, amount }),
+
   transferAccount: () => axiosInstance.post("/accounts/verification"),
 
   getAccountList: () => axiosInstance.get("/accounts"),
