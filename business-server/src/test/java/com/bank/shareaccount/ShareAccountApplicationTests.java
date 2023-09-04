0dd70d3d70ce1faba9cae33eb11a19b6b5b30240
jongmyung <lanto12@naver.com>
2023-09-04 20:14:24 +0900

    :bug: Fix : ci 를 위한 test 주석 처리

--- business-server/src/test/java/com/bank/shareaccount/ShareAccountApplicationTests.java
+++ business-server/src/test/java/com/bank/shareaccount/ShareAccountApplicationTests.java
@@ -1,13 +1,13 @@
-package com.bank.shareaccount;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ShareAccountApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.bank.shareaccount;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ShareAccountApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
