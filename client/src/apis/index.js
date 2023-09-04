86344d5cb2dfe559640061d41c78be62b2b90d18
Seokjun7074 <sukjune96@gmail.com>
2023-09-04 15:36:34 +0900

    :sparkles: Feat : 로그인 시 토큰 저장

--- client/src/apis/index.js
+++ client/src/apis/index.js
@@ -1,9 +1,11 @@
 import axios from "axios";
+import { getSession } from "utils/session";
 
 export const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_SERVER_URL,
   headers: {
     "Content-Type": "application/json",
+    Authorization: `Bearer ${getSession("Authorization")}`,
   },
 });
 
--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -10,7 +10,11 @@ const LoginPage = () => {
   const [passwordInput, setPasswordInput, passwordHandler] = useInput("");
   const loginMutation = useMutation({
     mutationFn: userAPI.login,
-    onSuccess: () => {
+    onSuccess: (data, variables, context) => {
+      const token = context.response.headers.get("Authorization");
+      const refresh = context.response.headers.get("Authorization-refresh");
+      sessionStorage.setItem("Authorization", token);
+      sessionStorage.setItem("Authorization-refresh", refresh);
       navigate("/main");
     },
   });
@@ -28,7 +32,11 @@ const LoginPage = () => {
       <S.LoginText> </S.LoginText>
       <S.InputWrapper>
         <S.InputBox placeholder="이메일" type="email" onChange={idHandler} />
-        <S.InputBox placeholder="비밀번호" type="password" onChange={passwordHandler} />
+        <S.InputBox
+          placeholder="비밀번호"
+          type="password"
+          onChange={passwordHandler}
+        />
       </S.InputWrapper>
       <S.NextButton onClick={submitSignUp}>로그인</S.NextButton>
     </S.LoginPageWrapper>
