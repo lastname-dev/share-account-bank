a67579b1ac6b9b9e88b7a487de470aa185d592b1
Choi Hwi bin <chlgnlqls3@nate.com>
2023-09-05 21:16:01 +0900

    ✨Feat: 초대 페이지 UI

--- client/src/pages/InvitationPage/InvitationPage.style.js
+++ client/src/pages/InvitationPage/InvitationPage.style.js
@@ -1,4 +1,39 @@
+import Button from "components/@common/Button/Button";
 import Flex from "components/@common/Flex/Flex";
 import { styled } from "styled-components";
 
-export const InvitationPageWrapper = styled(Flex)``;
+export const InvitationPageWrapper = styled(Flex)`
+  flex-direction: column;
+  justify-content: space-around;
+  height: 100vh;
+  padding: 9rem 0;
+`;
+
+export const InvitationMessage = styled.span`
+  color: ${({ theme }) => theme.color.darkgray};
+  font-size: ${({ theme }) => theme.fontSize.m};
+  font-weight: bold;
+`;
+
+export const InvitationImageContainer = styled(Flex)`
+  margin: 0 auto;
+  width: 60%;
+`;
+
+export const InvitationImage = styled.img`
+  width: 100%;
+`;
+
+export const InvitationInfoText = styled.span`
+  color: ${({ theme }) => theme.color.lightgray};
+  font-size: ${({ theme }) => theme.fontSize.s};
+  position: relative;
+  bottom: 4rem;
+`;
+
+export const InvitationButton = styled(Button)`
+  width: 60%;
+  background-color: ${({ theme }) => theme.color.primary};
+  color: ${({ theme }) => theme.color.white};
+  font-size: ${({ theme }) => theme.fontSize.m};
+`;
