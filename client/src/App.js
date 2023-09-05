41d0b01dabe388f3931d131bbc186f68b3318664
이석준 <sukjune96@gmail.com>
2023-09-05 22:27:58 +0900

    :recycle: Refactor : react-query retry 횟수 제한

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,13 @@ import AppRouter from "router/AppRouter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: 1,
+      },
+    },
+  });
 
   return (
     <React.Fragment>
