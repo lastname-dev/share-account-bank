62f109dde42213a6ac1af3c5224a4e6a2bfe1d12
Choi Hwi bin <chlgnlqls3@nate.com>
2023-09-06 18:23:58 +0900

    ✨Feat: 정산페이지UI_2

--- client/src/pages/CalculationPage/CalculationPage.style.js
+++ client/src/pages/CalculationPage/CalculationPage.style.js
@@ -8,3 +8,31 @@ export const CalculationPageWrapper = styled(Flex)`
   height: 100vh;
   padding: 9rem 0;
 `;
+
+export const CalculationMessage = styled.span`
+  color: ${({ theme }) => theme.color.darkgray};
+  font-size: ${({ theme }) => theme.fontSize.l};
+  font-weight: bold;
+`;
+
+export const CalculationImageContainer = styled(Flex)`
+  margin: 0 auto;
+  width: 60%;
+`;
+
+export const CalculationImage = styled.img`
+  width: 100%;
+`;
+export const CalculationInfoText = styled.span`
+  color: ${({ theme }) => theme.color.lightgray};
+  font-size: ${({ theme }) => theme.fontSize.s};
+  position: relative;
+  bottom: 4rem;
+`;
+
+export const CalculationButton = styled(Button)`
+  width: 60%;
+  background-color: ${({ theme }) => theme.color.primary};
+  color: ${({ theme }) => theme.color.white};
+  font-size: ${({ theme }) => theme.fontSize.m};
+`;
