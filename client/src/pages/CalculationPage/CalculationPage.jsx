58de4bdc591fae95c3e6f56ba05b6dd427649aea
Choi Hwi bin <chlgnlqls3@nate.com>
2023-09-06 18:23:06 +0900

    ✨Feat: 정산페이지 UI

---
+++ client/src/pages/CalculationPage/CalculationPage.jsx
@@ -0,0 +1,7 @@
+import * as S from "./CalculationPage.style";
+
+const CalculationPage = () => {
+  return <S.CalculationPageWrapper></S.CalculationPageWrapper>;
+};
+
+export default CalculationPage;
---
+++ client/src/pages/CalculationPage/CalculationPage.style.js
@@ -0,0 +1,10 @@
+import Button from "components/@common/Button/Button";
+import Flex from "components/@common/Flex/Flex";
+import { styled } from "styled-components";
+
+export const CalculationPageWrapper = styled(Flex)`
+  flex-direction: column;
+  justify-content: space-around;
+  height: 100vh;
+  padding: 9rem 0;
+`;
