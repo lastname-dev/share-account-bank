a1c8e4a6bc42320b96a62dbf6c32f0350ee92b57
Seokjun7074 <sukjune96@gmail.com>
2023-09-06 17:38:09 +0900

    ✨ Feat : 로그인 성공 시 이메일 저장

--- client/src/hooks/apiHook/useLogInMutation.js
+++ client/src/hooks/apiHook/useLogInMutation.js
@@ -8,9 +8,10 @@ export const useLogInMutation = () => {
 
   const loginMutation = useMutation({
     mutationFn: userAPI.login,
-    onSuccess: ({ data }) => {
+    onSuccess: ({ data }, variables) => {
       sessionStorage.setItem("Authorization", data.accessToken);
       sessionStorage.setItem("Authorization-refresh", data.refreshToken);
+      sessionStorage.setItem("userEmail", variables.id);
       navigate(PATH.ROOT);
     },
     onError: () => {
