2c3bbe32c33481d20cfa9076950b522a10721dc8
이석준 <sukjune96@gmail.com>
2023-09-10 02:38:28 +0900

    :sparkles: Feat : 회비납입여부 분기 처리

--- client/src/pages/TravelInfoPage/TravelInfoPage.jsx
+++ client/src/pages/TravelInfoPage/TravelInfoPage.jsx
@@ -1,13 +1,17 @@
+import { useLocation } from "react-router-dom";
 import * as S from "./TravelInfoPage.style";
 
 const TravelInfoPage = () => {
+  const { state } = useLocation();
+  const group = state?.group;
+
   return (
     <S.TravelInfoPageWrapper>
       <S.TravelInfoImageContainer>
         <S.TravelInfoImage src={process.env.PUBLIC_URL + "/image/airplane.png"} alt="logo" />
       </S.TravelInfoImageContainer>
       <S.MessageWrapper>
-        <S.TravelInfoMessage>목표 금액 90% 달성!</S.TravelInfoMessage>
+        <S.TravelInfoMessage>목표 금액 {(group.balance / group.goal) * 100}% 달성!</S.TravelInfoMessage>
         <S.TravelInfoText>설레는 여행이 다가와요!</S.TravelInfoText>
       </S.MessageWrapper>
       <S.TravelInfoButton>모임통장 관리하기</S.TravelInfoButton>
