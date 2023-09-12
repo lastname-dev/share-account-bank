93bff3eb15d1c8119df77bc67beb013431c99c2b
이석준 <sukjune96@gmail.com>
2023-09-13 00:58:16 +0900

    :sparkles: Feat : 초대링크 생성 페이지로 이동

--- client/src/pages/GroupPage/GroupPage.jsx
+++ client/src/pages/GroupPage/GroupPage.jsx
@@ -1,12 +1,18 @@
 import GroupDetail from "components/group/GroupDetail/GroupDetail";
 import * as S from "./GroupPage.style";
 import GroupAccountDetail from "components/group/GroupAccountDetail/GroupAccountDetail";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useGroupQuery } from "hooks/apiHook/useGroupQuery";
+import { PATH } from "constants/path";
 
 const GroupPage = () => {
+  const navigate = useNavigate();
   const { groupId } = useParams();
   const { groupData } = useGroupQuery(groupId);
+
+  const handleNavigate = () => {
+    navigate(PATH.INVITATION_PAGE(groupId));
+  };
 
   return (
     <>
@@ -25,6 +31,7 @@ const GroupPage = () => {
           limitMember={groupData.data.limitMember}
           participants={groupData.data.limitMember}
         />
+        <S.GroupInviteButton onClick={handleNavigate}>친구 초대하기</S.GroupInviteButton>
       </S.GroupPageWrapper>
     </>
   );
