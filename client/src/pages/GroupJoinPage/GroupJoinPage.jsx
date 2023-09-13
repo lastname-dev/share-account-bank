219519c43be328b01be9fdd190fb23ce3cad531b
jongmyung <lanto12@naver.com>
2023-09-13 22:11:48 +0900

    :art: Feat : 그룹 초대장 페이지

--- client/src/pages/GroupJoinPage/GroupJoinPage.jsx
+++ client/src/pages/GroupJoinPage/GroupJoinPage.jsx
@@ -2,13 +2,14 @@ import * as S from "./GroupJoinPage.style";
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import businessAPI from "apis/business";
-
+import InviteTicket from "components/invite/InviteTicket";
 const GroupJoinPage = () => {
   const link = useLocation().pathname.split("/joinGroup/")[1]; // 현재 url path에서 /invite/를 빼고 groupName을 추출
 
-  const [GroupName, setGroupName] = useState("");
+  const [groupName, setGroupName] = useState("");
   const [GroupId, setGroupId] = useState(1);
   const [showModal, setShowModal] = useState(false);
+  const [group, setGroup] = useState();
 
   useEffect(() => {
     const groupName = async () => {
@@ -17,6 +18,7 @@ const GroupJoinPage = () => {
         console.log(response.data);
         setGroupName(response.data.groupName);
         setGroupId(response.data.groupId);
+        setGroup(response.data);
       } catch (error) {
         console.error("초대장 출처 확인 :", error);
       }
@@ -26,22 +28,21 @@ const GroupJoinPage = () => {
 
   const joinGroup = async () => {
     try {
-      const response = await businessAPI.joinGroup(GroupName, link);
+      const response = await businessAPI.joinGroup(groupName, link);
     } catch (error) {
       console.error("가입 오류:", error);
     }
   };
 
   return (
-    <S.GroupJoinPageWrapper>
-      <S.GroupJoinMessage>
-        <br /> <h3>{GroupName} 초대장</h3>
-      </S.GroupJoinMessage>
-      <S.GroupJoinImageContainer></S.GroupJoinImageContainer>
-      <S.GroupJoinButton onClick={() => joinGroup()}>수락</S.GroupJoinButton>
-      {/* <S.ModalOverlay $show={showModal} onClick={() => setShowModal(false)} /> */}
-      {/* <S.ModalContainer $show={showModal}></S.ModalContainer> */}
-    </S.GroupJoinPageWrapper>
+    <>{group && <InviteTicket group={group} />}</>
+    // <S.GroupJoinPageWrapper>
+    // //   <S.GroupJoinMessage>
+    // //     <br /> <h3>{GroupName} 초대장</h3>
+    // //   </S.GroupJoinMessage>
+    // //   <S.GroupJoinImageContainer></S.GroupJoinImageContainer>
+    //   <S.GroupJoinButton onClick={() => joinGroup()}>수락</S.GroupJoinButton>
+    // // </S.GroupJoinPageWrapper>
   );
 };
 
