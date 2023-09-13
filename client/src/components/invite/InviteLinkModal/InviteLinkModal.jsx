d246dd4ab53363577a16afafcc9b5825a62ab206
이석준 <sukjune96@gmail.com>
2023-09-13 22:05:30 +0900

    :sparkles: Feat : 초대링크 클립보드에 복사

--- client/src/components/invite/InviteLinkModal/InviteLinkModal.jsx
+++ client/src/components/invite/InviteLinkModal/InviteLinkModal.jsx
@@ -1,19 +1,19 @@
-import { useState } from "react";
-import * as S from "./InviteLinkModal.style";
-import Flex from "components/@common/Flex/Flex";
+// import { useState } from "react";
+// import * as S from "./InviteLinkModal.style";
+// import Flex from "components/@common/Flex/Flex";
 
-const InviteLinkModal = ({ link }) => {
-    return (
-        <S.ModalWrapper>
-            <S.ModalContainer>
-                <h2>초대 링크</h2>
-                <h5 style={{ color: "red" }}>
-                    초대링크는 1회만 사용 가능합니다.
-                </h5>
-                <h3>{link}</h3>
-            </S.ModalContainer>
-        </S.ModalWrapper>
-    );
-};
+// const InviteLinkModal = ({ link }) => {
+//     return (
+//         <S.ModalWrapper>
+//             <S.ModalContainer>
+//                 <h2>초대 링크</h2>
+//                 <h5 style={{ color: "red" }}>
+//                     초대링크는 1회만 사용 가능합니다.
+//                 </h5>
+//                 <h3>{link}</h3>
+//             </S.ModalContainer>
+//         </S.ModalWrapper>
+//     );
+// };
 
-export default InviteLinkModal;
+// export default InviteLinkModal;
--- client/src/pages/InvitationPage/InvitationPage.jsx
+++ client/src/pages/InvitationPage/InvitationPage.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import businessAPI from "apis/business";
 import InviteLinkModal from "components/invite/InviteLinkModal/InviteLinkModal";
+import { toastSuccess } from "utils/toast";
 
 const InvitationPage = () => {
   const id = useLocation().pathname.match(/\/invite\/(\w+)/)[1]; // 현재 url path에서 /invite/를 빼고 groupName을 추출
@@ -13,9 +14,7 @@ const InvitationPage = () => {
     try {
       const response = await businessAPI.makeJoinLink(id);
       const link = response.data;
-      console.log(link);
-      setInvitationLink(link);
-      setShowModal(true);
+      navigator.clipboard.writeText(link).then(() => toastSuccess("클립보드에 복사되었습니다."));
     } catch (error) {
       console.error("초대 링크 생성 오류:", error);
     }
@@ -31,10 +30,10 @@ const InvitationPage = () => {
         <S.InvitationInfoText>클릭해서 초대링크 생성</S.InvitationInfoText>
       </S.InvitationImageContainer>
       <S.InvitationButton>완료</S.InvitationButton>
-      <S.ModalOverlay $show={showModal} onClick={() => setShowModal(false)} />
+      {/* <S.ModalOverlay $show={showModal} onClick={() => setShowModal(false)} />
       <S.ModalContainer $show={showModal}>
         <InviteLinkModal link={invitationLink} />
-      </S.ModalContainer>
+      </S.ModalContainer> */}
     </S.InvitationPageWrapper>
   );
 };
