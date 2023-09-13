6342f15fad91cb2988ee30ae4c5134059bae099d
이석준 <sukjune96@gmail.com>
2023-09-13 19:40:55 +0900

    :sparkles: Feat : 여행 기록 조회

--- client/src/pages/TravelLogPage/TravelLogPage.jsx
+++ client/src/pages/TravelLogPage/TravelLogPage.jsx
@@ -1,7 +1,53 @@
+import { useTravelLogsQuery } from "hooks/apiHook/useTravelLogsQuery";
 import * as S from "pages/TravelLogPage/TravelLogPage.style";
+import { useCallback } from "react";
 
 const TravelLogPage = () => {
-  return <S.TravelLogPageWrapper></S.TravelLogPageWrapper>;
+  const travelLogsQuery = useTravelLogsQuery();
+  const logList = travelLogsQuery?.travelLogsData?.data;
+
+  const convertDateToText = useCallback((date) => {
+    const dateArr = date.split("-");
+    return String(dateArr[0]) + "년 " + String(parseInt(dateArr[1])) + "월의 추억";
+  }, []);
+
+  const dummy = [
+    {
+      photo: "photo",
+      travelId: 1,
+      groupName: "groupName",
+      startDate: "startDate",
+      endDate: "endDate",
+    },
+    {
+      photo: "photo",
+      travelId: 2,
+      groupName: "groupName",
+      startDate: "startDate",
+      endDate: "endDate",
+    },
+    {
+      photo: "photo",
+      travelId: 3,
+      groupName: "groupName",
+      startDate: "startDate",
+      endDate: "endDate",
+    },
+  ];
+  return (
+    <S.TravelLogPageWrapper>
+      <S.TravelGrid>
+        {logList?.map((item) => (
+          <S.TravelCard key={item.travelId}>
+            <S.ImageContainer>
+              <S.TravelImage src={item.photo} />
+            </S.ImageContainer>
+            <S.TravelCardButton>{convertDateToText(item.startDate)}</S.TravelCardButton>
+          </S.TravelCard>
+        ))}
+      </S.TravelGrid>
+    </S.TravelLogPageWrapper>
+  );
 };
 
 export default TravelLogPage;
--- client/src/pages/TravelLogPage/TravelLogPage.style.js
+++ client/src/pages/TravelLogPage/TravelLogPage.style.js
@@ -5,3 +5,39 @@ export const TravelLogPageWrapper = styled(Flex)`
   width: 100%;
   padding: 4rem 3rem;
 `;
+
+export const TravelGrid = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 1rem;
+`;
+
+export const TravelCard = styled(Flex)`
+  height: 30rem;
+  flex-direction: column;
+  justify-content: start;
+  align-items: start;
+  gap: 1rem;
+  border-radius: 10px;
+  overflow: hidden;
+  background-color: ${({ theme }) => theme.color.offwhite};
+  cursor: pointer;
+  box-shadow: 0px 0px 2px 2px ${({ theme }) => theme.color.offwhite};
+  font-weight: bold;
+  font-size: ${({ theme }) => theme.fontSize.s};
+`;
+export const ImageContainer = styled(Flex)`
+  width: 100%;
+  height: 20rem;
+  justify-content: start;
+`;
+export const TravelImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+export const TravelCardButton = styled(Flex)`
+  width: 100%;
+  height: calc(100% - 20rem);
+`;
