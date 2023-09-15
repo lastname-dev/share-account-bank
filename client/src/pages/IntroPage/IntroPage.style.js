d1bdb0511e736ef943b31a7007e7483cdcf65235
최종명 <lanto12@naver.com>
2023-09-15 16:16:46 +0900

    :sparkles: Feat : 메인화면 ui 수정

--- client/src/pages/IntroPage/IntroPage.style.js
+++ client/src/pages/IntroPage/IntroPage.style.js
@@ -1,21 +1,35 @@
 import Button from "components/@common/Button/Button";
 import Flex from "components/@common/Flex/Flex";
-import { styled } from "styled-components";
+import { styled, keyframes } from "styled-components";
+
+const rotate = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
 
 export const IntroPageWrapper = styled(Flex)`
   flex-direction: column;
   justify-content: space-around;
   height: 100vh;
   padding: 9rem 0;
+  /* background-color: #bfe4ff; */
+  background: linear-gradient(#b2ccff, aliceblue);
+  background-image: img;
 `;
 
 export const LogoContainer = styled(Flex)`
   margin: 0 auto;
-  width: 60%;
+  width: 40%;
 `;
 
 export const LogoImage = styled.img`
-  width: 100%;
+  width: 80%;
+  animation: ${rotate} 3s linear infinite;
+  border-radius: 50%;
 `;
 
 export const ButtonWrapper = styled(Flex)`
@@ -26,9 +40,12 @@ export const ButtonWrapper = styled(Flex)`
 
 export const LoginButton = styled(Button)`
   width: 60%;
+  height: 40%;
   background-color: ${({ theme }) => theme.color.primary};
+  /* background-color: #f1f1f1; */
   color: ${({ theme }) => theme.color.white};
-  font-size: ${({ theme }) => theme.fontSize.l};
+  font-size: ${({ theme }) => theme.fontSize.m};
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); /* 그림자 효과 */
 `;
 
 export const SigninButton = styled(LoginButton)``;
