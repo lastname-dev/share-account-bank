7ebf7b23a449904bac9f6ec72408cf25f5fba35a
이석준 <sukjune96@gmail.com>
2023-09-17 03:04:11 +0900

    :sparkles: Feat : 여행 로그 상세 조회 페이지

---
+++ client/src/pages/TravelLogDetailPage/TravelLogDetailPage.style.js
@@ -0,0 +1,7 @@
+import Flex from "components/@common/Flex/Flex";
+import { styled } from "styled-components";
+
+export const TravelLogDetailPageWrapper = styled(Flex)`
+  padding: 2rem 0;
+  flex-direction: column;
+`;
